fe2c770c6e684db26b85b39c3be65e1676458e8f
xpym <xpym2406@gmail.com>
2025-07-20 01:38:41 +0300

    Fixed

--- TrayTemperature/DynamicIcon.cs
+++ TrayTemperature/DynamicIcon.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Text;
+using System.Runtime.InteropServices;
 
 namespace TrayTemperature
 {
@@ -8,23 +9,43 @@ namespace TrayTemperature
     {
         private static readonly Font CachedFont = new Font("Consolas", 7);
 
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        extern static bool DestroyIcon(IntPtr handle);
+
         public static Icon CreateIcon(string Line1Text, Color Line1Color, string Line2Text, Color Line2Color)
         {
             using (var bitmap = new Bitmap(16, 16))
-            using (var graph = Graphics.FromImage(bitmap))
             {
-                graph.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
-
-                using (var brush1 = new SolidBrush(Line1Color))
-                using (var brush2 = new SolidBrush(Line2Color))
+                using (var graph = Graphics.FromImage(bitmap))
                 {
-                    graph.DrawString(Line1Text, CachedFont, brush1, new PointF(-1, -3));
-                    graph.DrawString(Line2Text, CachedFont, brush2, new PointF(-1, 7));
+                    graph.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+                    using (var brush1 = new SolidBrush(Line1Color))
+                    using (var brush2 = new SolidBrush(Line2Color))
+                    {
+                        graph.DrawString(Line1Text, CachedFont, brush1, new PointF(-1, -3));
+                        graph.DrawString(Line2Text, CachedFont, brush2, new PointF(-1, 7));
+                    }
                 }
 
+                // Получаем handle иконки
                 var hIcon = bitmap.GetHicon();
-                var icon = Icon.FromHandle(hIcon);
-                return (Icon)icon.Clone();
+
+                try
+                {
+                    // Создаем иконку из handle и сразу клонируем
+                    using (var tempIcon = Icon.FromHandle(hIcon))
+                    {
+                        return new Icon(tempIcon, 16, 16);
+                    }
+                }
+                finally
+                {
+                    // Обязательно освобождаем handle
+                    if (hIcon != IntPtr.Zero)
+                    {
+                        DestroyIcon(hIcon);
+                    }
+                }
             }
         }
 
